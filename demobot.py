40c33477d64097c2a80328347b6169898f9b2660
Akee Elliott <aell7798@SHARED.SYDNEY.EDU.AU>
2019-01-10 12:11:04 +1100

    Added findFactors to the Slack API.

--- demobot.py
+++ demobot.py
@@ -17,6 +17,21 @@ def apiSlack_isPrime () :
 
     return f"{usr_num} is a prime number."
 
+# Discovers all factors of given number.
+def apiSlack_findFactors () :
+    usr_num = request.values.get('text')
+    usr_num = int(usr_num)
+    usr_numFactors = []
+
+    for n in range(2, ceil(usr_num ** 0.5)) : #Square root of usr_num.
+        if usr_num % n == 0 :
+            usr_numFactors.append(n)
+    
+    if len(usr_numFactors) == 0 :
+        return f"{usr_num} is a prime number."
+    else :
+        return f"{usr_num} has the factors {usr_numFactors}."
+
 if __name__ == '__main__' :
     # Start the web server!
     app.run()
