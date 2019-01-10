3d443227e038d05cbf2dc153306ca8419935b9be
Akee Elliott <aell7798@SHARED.SYDNEY.EDU.AU>
2019-01-10 11:48:54 +1100

    Fixed syntax errors for isPrime.

--- demobot.py
+++ demobot.py
@@ -10,13 +10,13 @@ def apiSlack_isPrime () :
     usr_num = request.value.get('text')
     usr_num = int(usr_num)
 
-    for n in range(usr_num ** 0.5) #Square root of usr_num.
-        if usr_num % n == 0
+    for n in range(usr_num ** 0.5) : #Square root of usr_num.
+        if usr_num % n == 0 :
             return f"{usr_num} is not a prime number, {n} is a factor."
 
     return f"{usr_num} is a prime number."
 
-if __name__ == '__main__':
+if __name__ == '__main__' :
     # Start the web server!
     app.run()
 
