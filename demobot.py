dcdb727fc2d37cb09436551c6d426d33395153fe
Akee Elliott <aell7798@SHARED.SYDNEY.EDU.AU>
2019-01-10 12:19:58 +1100

    Improved mathematical logic and presentation of findFunction.

--- demobot.py
+++ demobot.py
@@ -11,7 +11,7 @@ def apiSlack_isPrime () :
     usr_num = request.values.get('text')
     usr_num = int(usr_num)
 
-    for n in range(2, ceil(usr_num ** 0.5)) : #Square root of usr_num.
+    for n in range(2, ceil(usr_num ** 0.5)) : #Square root of usr_num, rounded up.
         if usr_num % n == 0 :
             return f"{usr_num} is not a prime number, {n} is a factor."
 
@@ -24,14 +24,16 @@ def apiSlack_findFactors () :
     usr_num = int(usr_num)
     usr_numFactors = []
 
-    for n in range(2, ceil(usr_num ** 0.5)) : #Square root of usr_num.
+    for n in range(2, ceil(usr_num / 2 + 1)) : #One more then half of usr_num, rounded up.
         if usr_num % n == 0 :
             usr_numFactors.append(n)
     
     if len(usr_numFactors) == 0 :
-        return f"{usr_num} is a prime number."
+        return f"{usr_num} is a prime number, and has no distinct factors."
     else :
-        return f"{usr_num} has the factors {usr_numFactors}."
+        usr_numFactors = ", ".join(usr_numFactors)
+        usr_numFactors = usr_numFactors[:-2]
+        return f"{usr_num} has the distinct factors of {usr_numFactors}."
 
 if __name__ == '__main__' :
     # Start the web server!
