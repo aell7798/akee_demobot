f5f6c2f8c021d5baa0dde136536ae510fc589f91
Akee Elliott <aell7798@SHARED.SYDNEY.EDU.AU>
2019-01-10 12:36:37 +1100

    Fixed math error in isPrime.

--- demobot.py
+++ demobot.py
@@ -11,7 +11,7 @@ def apiSlack_isPrime () :
     usr_num = request.values.get('text')
     usr_num = int(usr_num)
 
-    for n in range(2, ceil(usr_num ** 0.5)) : #Square root of usr_num, rounded up.
+    for n in range(2, ceil(usr_num ** 0.5) + 1) : #Square root of usr_num plus one, rounded up.
         if usr_num % n == 0 :
             return f"{usr_num} is not a prime number, {n} is a factor."
 
@@ -24,7 +24,7 @@ def apiSlack_findFactors () :
     usr_num = int(usr_num)
     usr_numFactors = []
 
-    for n in range(2, ceil((usr_num / 2) + 2)) : #One more then half of usr_num, rounded up.
+    for n in range(2, ceil((usr_num / 2) + 2)) : #Two more then half of usr_num, rounded up.
         if usr_num % n == 0 :
             usr_numFactors.append(n)
     
