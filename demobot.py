9cdc6ab7c881a03ab28bec8bbebce02ddcfb02a0
Akee Elliott <aell7798@SHARED.SYDNEY.EDU.AU>
2019-01-10 12:26:32 +1100

    Fixed math error and presentation error for findFactors.

--- demobot.py
+++ demobot.py
@@ -24,7 +24,7 @@ def apiSlack_findFactors () :
     usr_num = int(usr_num)
     usr_numFactors = []
 
-    for n in range(2, ceil(usr_num / 2 + 1)) : #One more then half of usr_num, rounded up.
+    for n in range(2, ceil((usr_num / 2) + 1)) : #One more then half of usr_num, rounded up.
         if usr_num % n == 0 :
             usr_numFactors.append(n)
     
@@ -32,7 +32,7 @@ def apiSlack_findFactors () :
         return f"{usr_num} is a prime number, and has no distinct factors."
     else :
         usr_numFactors = ", ".join(map(str, usr_numFactors))
-        usr_numFactors = usr_numFactors[:-2]
+        usr_numFactors = usr_numFactors[:-3]
         return f"{usr_num} has the distinct factors of {usr_numFactors}."
 
 if __name__ == '__main__' :
