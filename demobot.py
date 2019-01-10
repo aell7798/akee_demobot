55a7d5858af87467d6af6c46900d91faeccdb2b6
Akee Elliott <aell7798@SHARED.SYDNEY.EDU.AU>
2019-01-10 12:41:18 +1100

    Fixed shared math error in both isPrime and findFactors.

--- demobot.py
+++ demobot.py
@@ -12,7 +12,7 @@ def apiSlack_isPrime () :
     usr_num = int(usr_num)
 
     for n in range(2, ceil(usr_num ** 0.5) + 1) : #Square root of usr_num plus one, rounded up.
-        if usr_num % n == 0 :
+        if usr_num % n and n != usr_num :
             return f"{usr_num} is not a prime number, {n} is a factor."
 
     return f"{usr_num} is a prime number."
@@ -25,7 +25,7 @@ def apiSlack_findFactors () :
     usr_numFactors = []
 
     for n in range(2, ceil((usr_num / 2) + 2)) : #Two more then half of usr_num, rounded up.
-        if usr_num % n == 0 :
+        if usr_num % n == 0 and n != usr_num :
             usr_numFactors.append(n)
     
     if len(usr_numFactors) == 0 :
